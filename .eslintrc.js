089fb2cd8b52baef8be9016578030112ca7e0ae4
vagusX <vagusxl@gmail.com>
2020-04-14 22:14:54 +0800

    feat: infra update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
-const { eslintTS, deepmerge } = require('@ice/spec');
-
-module.exports = deepmerge(eslintTS, {
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
   rules: {
-    "@typescript-eslint/no-var-requires": 0,
-
-    // 与 kaitian 保持一致
-    "@typescript-eslint/explicit-member-accessibility": 0,
-    "@typescript-eslint/no-non-null-assertion": 0,
-    "class-methods-use-this": 0,
-    "no-underscore-dangle": 0,
-    "dot-notation": 0,
-    'no-restricted-syntax': 0,
-    'prefer-promise-reject-errors': 0
-  }
-});
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
