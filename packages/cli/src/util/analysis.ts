31bdb798f45d5ad98b1d059d766366ce6874c6b0
茶旋 <chauxan.wh@antfin.com>
2020-07-21 19:52:59 +0800

    fix: fix lint

--- packages/cli/src/util/analysis.ts
+++ packages/cli/src/util/analysis.ts
@@ -1,5 +1,5 @@
-import get from 'lodash.get'
-import { safeParseJson } from './json'
+import get from 'lodash.get';
+import { safeParseJson } from './json';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -11,28 +11,26 @@ export const useKeyPathDirect = Symbol('useKeyPath');
 
 const isURL = (str: string): boolean => {
   try {
-    new URL(str)
-    return true
+    new URL(str);
+    return true;
   } catch {
-    return false
+    return false;
   }
 }
 
 const uniq = (arr: string[]): string[] => {
-  return Array.from(new Set(arr))
+  return Array.from(new Set(arr));
 }
 
 const flatten = (arr: any) => {
-  return arr.reduce((pre: any[], cur: any) => {
-    return pre.concat(Array.isArray(cur) ? flatten(cur) : cur)
-  }, [])
+  return arr.reduce((pre: any[], cur: any) => pre.concat(Array.isArray(cur) ? flatten(cur) : cur), []);
 }
 
 const touchedFiles: Set<string> = new Set();
 
 export const ANALYSIS_ENTRY_LIST = [
   'package.json',
-].map(entry => path.resolve(execBasePath, entry))
+].map(entry => path.resolve(execBasePath, entry));
 
 
 export const ANALYSIS_FIELD_PATH = {
@@ -50,15 +48,15 @@ export const ANALYSIS_FIELD_PATH = {
 }
 
 const getAppendResource = (obj: any): string[] => {
-  const fields = ['include']
+  const fields = ['include'];
   return fields.reduce((pre: string[], cur: string) => {
-    const next = get(obj, cur)
+    const next = get(obj, cur);
 
     if (typeof next === 'undefined') {
-      return pre
+      return pre;
     }
 
-    return flatten(pre.concat(next))
+    return flatten(pre.concat(next));
   }, [])
 }
 
@@ -66,10 +64,10 @@ const analysisSingleFile = (filepath: string) => {
 
   // 循环依赖
   if (touchedFiles.has(filepath)) {
-    return []
+    return [];
   }
 
-  touchedFiles.add(filepath)
+  touchedFiles.add(filepath);
 
   const dirPath = path.dirname(filepath);
 
@@ -79,54 +77,52 @@ const analysisSingleFile = (filepath: string) => {
 
   const appendResource: string[] = flatten(appendPath.length === 0 ? [] : (
     appendPath.map(p => analysisSingleFile(path.resolve(dirPath, p)))
-  ))
+  ));
 
   return appendResource.concat(Object.keys(ANALYSIS_FIELD_PATH)
     .reduce((preResult: string[], curPickPath: string) => {
 
-      const extraDesc: string | symbol = ANALYSIS_FIELD_PATH[curPickPath]
+      const extraDesc: string | symbol = ANALYSIS_FIELD_PATH[curPickPath];
 
-      const isArrayPath = extraDesc !== useKeyPathDirect
+      const isArrayPath = extraDesc !== useKeyPathDirect;
 
       const result: string | string[] = isArrayPath
         ? get(fileJsonObj, curPickPath, []).filter((i: any) => i[extraDesc]).map((i: any) => i[extraDesc])
         : get(fileJsonObj, curPickPath)
 
-      const skip = typeof result === 'undefined' || result.length === 0
+      const skip = typeof result === 'undefined' || result.length === 0;
 
-      const rawPath2AbsosultPath = (p: string) => {
-        return isURL(p) ? p : path.resolve(dirPath, p)
-      }
+      const rawPath2AbsosultPath = (p: string) => isURL(p) ? p : path.resolve(dirPath, p);
 
       return skip ? preResult : flatten(preResult.concat(
         isArrayPath
           ? (result as string[]).map((p) =>rawPath2AbsosultPath(p))
           : [rawPath2AbsosultPath(result as string)]
       ))
-    }, []))
+    }, []));
 }
 
 const replaceAbsolutePath2relative = (str: string) => {
-  return isURL(str) ? str : str.replace(execBasePath + '/', '')
+  return isURL(str) ? str : str.replace(execBasePath + '/', '');
 }
 
 
 // main
 export const buildWebAssetsMeta = () => {
   const result = uniq(flatten(ANALYSIS_ENTRY_LIST.map(filePath => analysisSingleFile(filePath))))
-    .map(p => replaceAbsolutePath2relative(p))
+    .map(p => replaceAbsolutePath2relative(p));
 
   const preFileContent = (() => {
     try {
       const fileStr = fs.readFileSync(metaFilePath, 'utf-8');
-      return safeParseJson(fileStr)
+      return safeParseJson(fileStr);
     } catch {
-      return {}
+      return {};
     }
-  })()
+  })();
 
   fs.writeFileSync(metaFilePath, JSON.stringify({
     ...preFileContent,
     "web-assets": result
-  }, null, 2))
+  }, null, 2));
 }
