d883535a6327f2b399e1fb9bb1afa572dafbde5e
vagusX <vagusxl@gmail.com>
2020-08-04 19:57:47 +0800

    fix: resolve.extensions for .ts in webpack.worker

--- packages/cli/src/scripts/webpack/webpack.config.worker.js
+++ packages/cli/src/scripts/webpack/webpack.config.worker.js
@@ -27,7 +27,7 @@ module.exports = (options = {}) => {
     .target('node')
     .end();
 
-  config.resolve.extensions.merge(['.js', 'ts']);
+  config.resolve.extensions.merge(['.js', '.ts']);
 
   config.optimization.minimize(false);
 
