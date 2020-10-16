ece3b0ce59e0cf001beac57c4ef1233ced796020
茶旋 <chauxan.wh@antfin.com>
2020-10-16 11:40:22 +0800

    feat: publish 时读取 package.json 中的 publisher

--- packages/cli/src/util/extension.ts
+++ packages/cli/src/util/extension.ts
@@ -1,11 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 import { promisify } from 'util';
+import { Package as NPMPackageJSON } from 'normalize-package-data';
+import { safeParseJson } from './json';
 
-export async function getExtPkgContent() {
-  const json = await promisify(fs.readFile)(
-    path.join(process.cwd(), 'package.json'),
-    'utf-8',
-  );
-  return JSON.parse(json);
+
+export interface ExtensionPackageJSON extends NPMPackageJSON {
+  publisher: string;
+}
+
+
+export async function getExtPkgContent(): Promise<ExtensionPackageJSON | undefined> {
+  try {
+    const json = await promisify(fs.readFile)(
+      path.join(process.cwd(), 'package.json'),
+      'utf-8',
+    );
+    return safeParseJson(json);
+  } catch(e) {
+    return undefined;
+  }
 }
