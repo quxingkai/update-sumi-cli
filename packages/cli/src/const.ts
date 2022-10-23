50f3205e7d07af985822ab4526c4cac99132f405
bk1012 <sijie.zsi@alibaba-inc.com>
2022-10-23 23:13:28 +0800

    chore: revert const.ts

---
+++ packages/cli/src/const.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+import os from 'os';
+
+export const npmClient = 'npm';
+
+export const enginePkgName = '';
+
+export const kaitianInfraDir = path.resolve(os.homedir(), '.opensumi-cli');
+
+export const marketplaceApiAddress = '';
+
+export const configYmlPath = path.resolve(kaitianInfraDir, 'config.yml');
+
+export const marketplaceAccountId = '';
+
+export const marketplaceMasterKey = '';
